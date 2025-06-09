3df96e8748b2a6afd31950923a03ff31f4af9612
israelkur-ayong <i.ayong@alustudent.com>
2025-06-09 13:11:37 -0700

    Initial commit

---
+++ create_branches.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make an initial commit if not already done
+git add .
+git commit -m "Initial commit"
+git branch -M master
+git push -u origin master
+
+# Branches to create
+branches=(
+  "feature/html-css"
+  "feature/gpa-logic"
+  "feature/dom-events"
+  "feature/bonus-features"
+  "docs/readme"
+)
+
+# Create and push each branch
+for branch in "${branches[@]}"
+do
+  git checkout -b $branch
+  git push -u origin $branch
+  git checkout master
+done
+
+echo "✅ All branches created and pushed!"
+i
